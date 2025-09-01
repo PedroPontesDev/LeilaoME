315fb61db8cc60aba6bc17c8f657984c50e05b04
Pedro Pontes <pedrohenriquepsantos2000@gmail.com>
2025-09-01 03:56:04 -0300

    Inicio do Projeto Com Pom Ja Quase Todo COnfigurado

---
+++ LeialaoME/src/main/java/com/devPontes/LeialaoME/LeialaoMeApplication.java
@@ -0,0 +1,13 @@
+package com.devPontes.LeialaoME;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeialaoMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeialaoMeApplication.class, args);
+	}
+
+}
---
+++ LeialaoME/src/test/java/com/devPontes/LeialaoME/LeialaoMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devPontes.LeialaoME;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeialaoMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
