11da297d12dceadc5e4ba3cfcb4babb2595c59cf
Pedro Pontes <pedrohenriquepsantos2000@gmail.com>
2025-09-01 04:03:25 -0300

    Entidade Usuario Sendo Criada Utilizando @Inheritance e sendo configurada com Spring Security

--- LeialaoME/src/main/java/com/devPontes/LeialaoME/Appplication.java
+++ LeialaoME/src/main/java/com/devPontes/LeialaoME/Appplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LeialaoMeApplication {
+public class Appplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LeialaoMeApplication.class, args);
+		SpringApplication.run(Appplication.class, args);
 	}
 
 }
