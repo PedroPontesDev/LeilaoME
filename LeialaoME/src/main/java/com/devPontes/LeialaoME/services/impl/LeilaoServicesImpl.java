bffa462c0dcc56f7dbd769fc82d54fa32205d6ca
Pedro Pontes <pedrohenriquepsantos2000@gmail.com>
2025-09-21 14:02:04 -0300

    LeilaoServices com alguns serviso implementados

--- LeialaoME/src/main/java/com/devPontes/LeialaoME/services/impl/LeilaoServicesImpl.java
+++ LeialaoME/src/main/java/com/devPontes/LeialaoME/services/impl/LeilaoServicesImpl.java
@@ -3,8 +3,11 @@ package com.devPontes.LeialaoME.services.impl;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,8 +17,9 @@ import com.devPontes.LeialaoME.exceptions.UsuarioNaoEncontradoException;
 import com.devPontes.LeialaoME.model.dto.LeilaoDTO;
 import com.devPontes.LeialaoME.model.dto.UsuarioCompradorDTO;
 import com.devPontes.LeialaoME.model.entities.Leilao;
+import com.devPontes.LeialaoME.model.entities.Oferta;
+import com.devPontes.LeialaoME.model.entities.UsuarioComprador;
 import com.devPontes.LeialaoME.model.entities.UsuarioVendedor;
-import com.devPontes.LeialaoME.model.entities.enums.StatusOferta;
 import com.devPontes.LeialaoME.model.entities.mapper.MyMaper;
 import com.devPontes.LeialaoME.repositories.LeilaoRepositories;
 import com.devPontes.LeialaoME.repositories.UsuarioCompradorRepositories;
@@ -39,7 +43,7 @@ public class LeilaoServicesImpl implements LeilaoServices {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
 	@Override
 	public LeilaoDTO criarLeilao(Long vendedorId, LeilaoDTO novoLeilao) {
 		UsuarioVendedor vendedor = vendedorRepository.findById(vendedorId)
@@ -50,36 +54,38 @@ public class LeilaoServicesImpl implements LeilaoServices {
 		leilaoRepository.save(leilao);
 		return MyMaper.parseObject(leilao, LeilaoDTO.class);
 	}
-	
-	@Override //ACHO Q ESTA ERRADO
+
+	@Override // ACHO Q ESTA ERRADO
 	public LeilaoDTO abrirLeilaoComPoucaMargemDeTempo(LeilaoDTO leilao, Long tempoMinimoHoras) {
 
-	    Leilao entity = MyMaper.parseObject(leilao, Leilao.class);
-	    
-	    if (!entity.isAindaAtivo())
-	        throw new LeilaoException("Leilão está inativo!");
+		Leilao entity = MyMaper.parseObject(leilao, Leilao.class);
 
-	    // Calcula duração atual
-	    Duration duracaoAtual = Duration.between(entity.getInicio(), entity.getTermino());
+		if (!entity.isAindaAtivo())
+			throw new LeilaoException("Leilão está inativo!");
 
-	    // Se o leilão já é menor que o mínimo, lança exceção
-	    if (duracaoAtual.toHours() < tempoMinimoHoras)
-	        throw new LeilaoException("O leilão não possui margem/tempo suficiente para abrir.");
+		// Calcula duração atual
+		Duration duracaoAtual = Duration.between(entity.getInicio(), entity.getTermino());
 
-	    //Se duracaoAtual do leilao for maior que tempo minimo horas eu seto o tempo calculado
-	    LocalDateTime tempoCalculado = entity.getInicio().plusHours(tempoMinimoHoras);
-	    if(duracaoAtual.toHours() > tempoMinimoHoras) {
-	    	entity.setTermino(tempoCalculado);
-	    }
-	    
-	    leilaoRepository.save(entity);
-	    
-	    return MyMaper.parseObject(entity, LeilaoDTO.class);
+		// Se o leilão já é menor que o mínimo, lança exceção
+		if (duracaoAtual.toHours() < tempoMinimoHoras)
+			throw new LeilaoException("O leilão não possui margem/tempo suficiente para abrir.");
+
+		// Se duracaoAtual do leilao for maior que tempo minimo esperado em horas eu
+		// seto o tempo
+		// calculado
+		LocalDateTime tempoCalculado = entity.getInicio().plusHours(tempoMinimoHoras);
+		if (duracaoAtual.toHours() > tempoMinimoHoras) {
+			entity.setTermino(tempoCalculado);
+		}
+
+		leilaoRepository.save(entity);
+
+		return MyMaper.parseObject(entity, LeilaoDTO.class);
 	}
 
 	@Override
 	public void fecharLeilao(Long leilaoId, String statusOferta) {
-		
+
 	}
 
 	@Override
@@ -89,11 +95,23 @@ public class LeilaoServicesImpl implements LeilaoServices {
 	}
 
 	@Override
-	//Precisa verificar a maior oferta dentro do leilao, verificar se esta inativo e setar o ganhador verificando qual é a oferta mais cara baseado
-	//Nas ofertas dadas pelos compradores
-	public UsuarioCompradorDTO definirGanhador(Long leilaoId) {
-		// TODO Auto-generated method stub
-		return null;
+	// Precisa verificar a maior oferta dentro do leilao, verificar se esta inativo
+	// e setar o ganhador verificando qual é a oferta mais cara baseado
+	// Nas ofertas dadas pelos compradores
+	public UsuarioCompradorDTO definirGanhador(Long leilaoId) throws Exception {
+		Leilao leilaoExistente = leilaoRepository.findById(leilaoId)
+				.orElseThrow(() -> new LeilaoException("Vendedor não encontrado com o ID" + leilaoId));
+		//Fazer nova validações
+		
+		// Garantir que existem ofertas
+	    List<Oferta> ofertas = leilaoExistente.getOfertas();
+	    if (ofertas == null || ofertas.isEmpty()) {
+	        throw new LeilaoException("Nenhuma oferta encontrada neste leilão.");
+	    }
+	    
+		Oferta maiorOferta = leilaoExistente.getOfertas().stream().max(null).get();
+
+
 	}
 
 	@Override
