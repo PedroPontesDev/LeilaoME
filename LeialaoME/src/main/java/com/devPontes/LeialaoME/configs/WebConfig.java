27466641ca4f6faa2f6189b558c62fff029fc96e
Pedro Pontes <pedrohenriquepsantos2000@gmail.com>
2025-10-31 05:02:05 -0300

    Configurando ContentNegotiation para servide não so json

--- LeialaoME/src/main/java/com/devPontes/LeialaoME/configs/WebConfig.java
+++ LeialaoME/src/main/java/com/devPontes/LeialaoME/configs/WebConfig.java
@@ -1,6 +1,8 @@
 package com.devPontes.LeialaoME.configs;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,6 +17,18 @@ public class WebConfig implements WebMvcConfigurer {
            .allowedHeaders("*")
            .allowCredentials(true);
    }
+
+	@Override
+	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+		configurer.ignoreAcceptHeader(false)
+				  .favorParameter(false)
+				  .mediaType("json", MediaType.APPLICATION_JSON)
+				  .mediaType("xml", MediaType.APPLICATION_XML)
+				  .defaultContentType(MediaType.APPLICATION_JSON);
+				  
+	}
+	
+	
 
 	
 	
--- LeialaoME/src/main/java/com/devPontes/LeialaoME/services/impl/PagamentoServiceImpl.java
+++ LeialaoME/src/main/java/com/devPontes/LeialaoME/services/impl/PagamentoServiceImpl.java
@@ -3,11 +3,11 @@ package com.devPontes.LeialaoME.services.impl;
 import java.util.UUID;
 
 import com.devPontes.LeialaoME.exceptions.LeilaoException;
-import com.devPontes.LeialaoME.exceptions.UsuarioNaoEncontradoException;
 import com.devPontes.LeialaoME.model.DTO.v1.PagamentoRequestDTO;
 import com.devPontes.LeialaoME.model.DTO.v1.PagamentoResponseDTO;
 import com.devPontes.LeialaoME.model.DTO.v1.WebHookPagamentoDTO;
 import com.devPontes.LeialaoME.model.entities.Leilao;
+import com.devPontes.LeialaoME.model.entities.Pagamento;
 import com.devPontes.LeialaoME.repositories.LeilaoRepositories;
 import com.devPontes.LeialaoME.repositories.PagamentosRepositories;
 import com.devPontes.LeialaoME.services.PagamentoService;
@@ -30,11 +30,15 @@ public class PagamentoServiceImpl implements PagamentoService {
 		
 		
 		
-		String txid = UUID.randomUUID().toString().concat(requestPix.getValor().toString());
+		String txid = UUID.randomUUID().toString();
 		String qrCode = "00020126580014BR.GOV.BCB.PIX0114chave-pix-fake5204000053039865406"
                    + requestPix.getValor() + "5802BR5920LeilaoMe Mock System";
 		
-		Pagamento pagamentoPix = 
+		Pagamento novoPagamento = new Pagamento();
+		novoPagamento.setTxid(txid);
+		novoPagamento.setQrCodeText(qrCode);
+		
+		return null;
 	
 	}
 
