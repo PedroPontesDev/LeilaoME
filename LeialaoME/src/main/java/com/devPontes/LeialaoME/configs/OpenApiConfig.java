c5ed9e399857d29a9c7dec72f760dbb6c29fd1f9
Pedro Pontes <pedrohenriquepsantos2000@gmail.com>
2026-04-26 14:34:03 -0300

    Swagger sendo instalaod, Controllers em Desenvolvimento!]

--- LeialaoME/src/main/java/com/devPontes/LeialaoME/configs/OpenApiConfig.java
+++ LeialaoME/src/main/java/com/devPontes/LeialaoME/configs/OpenApiConfig.java
@@ -4,13 +4,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 
 @Configuration
 public class OpenApiConfig {
 	
 	@Bean
 	public OpenAPI openApiConfig() {
-		return new OpenAPI();
+		return new OpenAPI()
+						.info(new Info()
+						.title("LeilaoME - Plataforma de Leilao Online!")
+						.version("V1")
+						.license(new License()
+						.name("Minha Licensa PUBLICA")
+						.url("http://localhost:8080")));
 	}
 	
 }
